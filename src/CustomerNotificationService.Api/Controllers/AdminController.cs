4c5cfe20517dea3c92e710457bdd8b30645e4f80
pieterhs <pieterhs@gmail.com>
2025-10-08 08:49:27 +0200

    docs: document admin template endpoints in Swagger and README

--- src/CustomerNotificationService.Api/Controllers/AdminController.cs
+++ src/CustomerNotificationService.Api/Controllers/AdminController.cs
@@ -6,30 +6,31 @@ namespace CustomerNotificationService.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Tags("Admin")]
 public class AdminController : ControllerBase
 {
     private readonly ITemplateService _templateService;
 
     public AdminController(ITemplateService templateService) => _templateService = templateService;
-
-    [HttpPost("templates/reload")]
-    public IActionResult ReloadTemplates()
-    {
-        // TODO: trigger template reload
-        return Ok(new { status = "reloaded" });
-    }
-
+    
     /// <summary>
-    /// List all notification templates.
+    /// Returns all notification templates.
     /// </summary>
+    /// <returns>A list of all notification templates in the system.</returns>
+    /// <response code="200">Successfully retrieved all templates.</response>
     [HttpGet("templates")]
     [ProducesResponseType(typeof(IEnumerable<TemplateDto>), StatusCodes.Status200OK)]
     public async Task<IActionResult> ListTemplates(CancellationToken ct)
         => Ok(await _templateService.GetAllAsync(ct));
 
     /// <summary>
-    /// Get a notification template by id.
+    /// Returns a single notification template by id.
     /// </summary>
+    /// <param name="id">The unique identifier of the template.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>The requested notification template.</returns>
+    /// <response code="200">Successfully retrieved the template.</response>
+    /// <response code="404">Template with the specified id was not found.</response>
     [HttpGet("templates/{id:guid}")]
     [ProducesResponseType(typeof(TemplateDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -40,8 +41,13 @@ public class AdminController : ControllerBase
     }
 
     /// <summary>
-    /// Create a new notification template.
+    /// Creates a new notification template.
     /// </summary>
+    /// <param name="request">The template data including name, channel, and content.</param>
+    /// <param name="ct">Cancellation token.</param>
+    /// <returns>The id of the newly created template.</returns>
+    /// <response code="201">Template created successfully.</response>
+    /// <response code="400">Invalid template data provided.</response>
     [HttpPost("templates")]
     [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -53,14 +59,18 @@ public class AdminController : ControllerBase
     }
 
     /// <summary>
-    /// Update an existing notification template.
+    /// Updates an existing notification template.
     /// </summary>
-    /// <param name="id">Template id to update.</param>
-    /// <param name="request">Fields to update: name, content or channel.</param>
+    /// <param name="id">The unique identifier of the template to update.</param>
+    /// <param name="request">The updated template data including name, channel, and content.</param>
     /// <param name="ct">Cancellation token.</param>
-    /// <returns>200 OK if updated, 404 if not found.</returns>
+    /// <returns>Confirmation of successful update.</returns>
+    /// <response code="200">Template updated successfully.</response>
+    /// <response code="400">Invalid template data provided.</response>
+    /// <response code="404">Template with the specified id was not found.</response>
     [HttpPut("templates/{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateTemplate([FromRoute] Guid id, [FromBody] TemplateDto request, CancellationToken ct)
     {
@@ -69,11 +79,13 @@ public class AdminController : ControllerBase
     }
 
     /// <summary>
-    /// Delete a notification template.
+    /// Deletes a notification template.
     /// </summary>
-    /// <param name="id">Template id to delete.</param>
+    /// <param name="id">The unique identifier of the template to delete.</param>
     /// <param name="ct">Cancellation token.</param>
-    /// <returns>204 No Content or 404 if not found.</returns>
+    /// <returns>Confirmation of successful deletion.</returns>
+    /// <response code="204">Template deleted successfully.</response>
+    /// <response code="404">Template with the specified id was not found.</response>
     [HttpDelete("templates/{id:guid}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
